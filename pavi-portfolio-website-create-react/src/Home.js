aa59a54b73bf334f3b2a3296cfae6a5acece98de
Srinivasan Gopalan <srinivasang87@gmail.com>
2019-12-26 13:53:28 -0500

    Completing Navbar content and UI changes; Attempting responsive font sizes

--- pavi-portfolio-website-create-react/src/Home.js
+++ pavi-portfolio-website-create-react/src/Home.js
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 
+import './custom.scss'
 import "./GoogleFonts.css"
 import "./core.css";
 import "./Home.css";
@@ -13,19 +14,31 @@ const MyNavBar = ({ children }) => {
     const [show, toggleShow] = useState(true);
     return (
         <Container>
-            <Navbar sticky="top" bg="light" expand="lg" >
+            <Navbar fixed="top" bg="light" expand="xl" >
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mx-auto">
                     <Nav.Link className="active font-weight-bold px-md-4 real-active" href="#home">Home</Nav.Link>
-                    <Nav.Link className="active px-md-4" href="#link">Link</Nav.Link>
-                    <NavDropdown title="Dropdown" className="active" id="basic-nav-dropdown">
-                        <NavDropdown.Item className="active px-md-4" href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item className="active px-md-4" href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item className="active px-md-4" href="#action/3.3">Something</NavDropdown.Item>
-                        
-                        <NavDropdown.Item className="active px-md-4" href="#action/3.4">Separated link</NavDropdown.Item>
+                    <NavDropdown className="active font-weight-bold px-md-4" title="Fashion Design" id="basic-nav-dropdown">
+                        <NavDropdown.Item className="active px-md-4" href="#action/3.1">Time Travel</NavDropdown.Item>
+                        <NavDropdown.Item className="active px-md-4" href="#action/3.2">Beaute Devastatrice</NavDropdown.Item>
+                        <NavDropdown.Item className="active px-md-4" href="#action/3.3">The Bull's Eye</NavDropdown.Item>
+                        <NavDropdown.Item className="active px-md-4" href="#action/3.4">Predator Plastic</NavDropdown.Item>
+                        <NavDropdown.Item className="active px-md-4" href="#action/3.4">Great Depression</NavDropdown.Item>
                     </NavDropdown>
+                    <NavDropdown className="active font-weight-bold px-md-4" title="Textile" id="basic-nav-dropdown">
+                        <NavDropdown.Item className="active px-md-4" href="#action/3.1">Embroidery</NavDropdown.Item>
+                        <NavDropdown.Item className="active px-md-4" href="#action/3.2">Abstract</NavDropdown.Item>
+                        <NavDropdown.Item className="active px-md-4" href="#action/3.3">Brush Strokes</NavDropdown.Item>
+                        <NavDropdown.Item className="active px-md-4" href="#action/3.4">Flowers</NavDropdown.Item>
+                        <NavDropdown.Item className="active px-md-4" href="#action/3.4">Reflections</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link className="active font-weight-bold px-md-4" href="#link">Tech Packs</Nav.Link>
+                    <NavDropdown className="active font-weight-bold px-md-4" title="Internships" id="basic-nav-dropdown">
+                        <NavDropdown.Item className="active px-md-4" href="#action/3.1">Bibhu Mohapatra</NavDropdown.Item>
+                        <NavDropdown.Item className="active px-md-4" href="#action/3.2">The Levy Group</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link className="active font-weight-bold px-md-4" href="#link">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
