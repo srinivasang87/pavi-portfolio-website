036b8ea2ee8c7739b8a621793738fd7db4363097
Srinivasan Gopalan <srinivasang87@gmail.com>
2020-01-05 19:15:47 -0500

    Making more images responsive

--- pavi-portfolio-website-create-react/src/Fashion-Design/BeauteD.js
+++ pavi-portfolio-website-create-react/src/Fashion-Design/BeauteD.js
@@ -120,7 +120,7 @@ const BeauteD = () => {
             {images.map((value, index) => {
                 return <Row>
                     <Col className="outerImages" onClick={() => { setShow(true); setModalCarouselIndex(index); } }>
-                        <a href="#"><img src={images[index].leBigImg} className="rounded mx-auto d-block img-fluid" alt="..." /></a>
+                        <a href="#"><img src={images[index].leBigImg} className="rounded mx-auto d-block makeItResponsive" alt="..." /></a>
                     </Col>
                 </Row>
             })}
@@ -137,7 +137,7 @@ const BeauteD = () => {
                 <Carousel activeIndex={modalCarouselIndex} onSelect={handleSelect} indicators={false} interval={null}>
                 {images.map((value, index) => {
                 return <Carousel.Item>
-                        <img className="rounded img-fluid mx-auto d-block" src={images[index].leBigImg} alt="..." />
+                        <img className="rounded w-100 mx-auto d-block" src={images[index].leBigImg} alt="..." />
                     </Carousel.Item>
                 })}
                 </Carousel>
--- pavi-portfolio-website-create-react/src/Home.js
+++ pavi-portfolio-website-create-react/src/Home.js
@@ -13,7 +13,7 @@ const Home = () => (
     <MyNavBar activeNavLink="home" />
     <Container className="home-container">
       <div className="mobile-image">
-        <img src="https://static.wixstatic.com/media/d01fe9_b22518db86e94a6d87fc8c0317bf22bc~mv2_d_4833_3108_s_4_2.jpg/v1/fill/w_560,h_366,al_c,q_80,usm_0.66_1.00_0.01/YC3A2680%20copy_JPG.webp" className="mx-auto d-block img-fluid"/>
+        <img src="https://static.wixstatic.com/media/d01fe9_b22518db86e94a6d87fc8c0317bf22bc~mv2_d_4833_3108_s_4_2.jpg/v1/fill/w_560,h_366,al_c,q_80,usm_0.66_1.00_0.01/YC3A2680%20copy_JPG.webp" className="mx-auto d-block w-100"/>
       </div>
       <h1 className="display-4 d-flex justify-content-start em-wide home-title">Pavithra <br/> Rangarajan</h1>
     </Container>
